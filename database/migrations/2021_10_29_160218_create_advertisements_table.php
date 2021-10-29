93160b5097fb39781c9d4e31cf3b53b9d5775637
augtal <augustas.talacka@gmail.com>
2021-10-29 19:07:48 +0300

    Added advertisement table to database

---
+++ database/migrations/2021_10_29_160218_create_advertisements_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdvertisementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('advertisements', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->integer('year',);
+            $table->integer('mileage');
+            $table->float('price');
+            $table->string('make_model');
+            $table->string('fuel');
+            $table->string('body_type');
+            $table->integer('views');
+            $table->text('description');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('advertisements');
+    }
+}
